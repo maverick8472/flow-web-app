e36a129fbfe08d780808e306afae22c1f93ea1b0
maverick8472 <bassline_8472@yahoo.com>
2019-12-04 11:02:36 +0100

    Back2Top v1

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,6 @@
 
 
-
+// Toglle bars
 $(function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
@@ -12,6 +12,26 @@ $(function() {
         }
     });
 });
+
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
 
 
 
